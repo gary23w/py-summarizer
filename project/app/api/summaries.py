7c11508dcf88744af8a93ce1e5bf5c085d34867e
gary23w <garystimpson03@gmail.com>
2020-09-01 11:35:57 -0400

    test

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -26,7 +26,6 @@ async def create_summary(
     background_tasks.add_task(generate_summary, summary_id, payload.url)
 
     response_object = {"id": summary_id, "url": payload.url}
-    delete_posted_summary(summary_id)
 
     return response_object
 
@@ -66,7 +65,3 @@ async def update_summary(
         raise HTTPException(status_code=404, detail="Summary not found")
 
     return summary
-
-async def delete_posted_summary(id):
-    await asyncio.sleep(3600)
-    crud.delete(id)
