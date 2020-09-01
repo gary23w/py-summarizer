265fdf33a6b864c36479b599fc46c9b080dc780e
gary23w <garystimpson03@gmail.com>
2020-09-01 16:02:10 -0400

    'quick'

--- project/app/main.py
+++ project/app/main.py
@@ -23,13 +23,7 @@ def create_application() -> FastAPI:
 
 app = create_application()
 
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
+
 
 
 @app.on_event("startup")
