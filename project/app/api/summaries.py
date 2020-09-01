366a4e170fbfc39ea3fc61bb182d47021700c2de
gary23w <garystimpson03@gmail.com>
2020-09-01 19:22:23 -0400

    'quick'

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -22,8 +22,10 @@ async def create_summary(
     payload: SummaryPayloadSchema, background_tasks: BackgroundTasks
 ) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
+
     background_tasks.add_task(generate_summary, summary_id, payload.url)
-    response_object = {"id": summary_id, "url": payload.url}
+    response_object = {"id": summary_id, "url": payload.url, "summary": scummary}
+    read_summary(summary_id)
 
     return response_object
 
